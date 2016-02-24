7dd3b2e2f092d131a8d93f63b07d5b297cf83feb
grigori <grigori@grik.net>
2016-02-24 05:07:14 +0200

    added postgres, ev, apcu extensions

--- php_extensions_setup.sh
+++ php_extensions_setup.sh
@@ -6,9 +6,9 @@
 apt-get update && apt-get install -y --no-install-recommends \
         libxml2 libxml2-dev libxslt1.1 libxslt1-dev \
         libmemcached11 libmemcachedutil2 libmemcached-dev \
-        libssl-dev libcurl4-openssl-dev libreadline6-dev \
-        zlib1g-dev libbz2-dev xz-utils lbzip2 git \
-        postgresql-server-dev-9.4 libpq5
+        libssl-dev libcurl4-openssl-dev libreadline6-dev libgeoip-dev libgeoip1 \
+        libc-client2007e libkrb5-3 libc-client2007e-dev libkrb5-dev \
+        zlib1g-dev libbz2-dev xz-utils lbzip2 git wget
 
 # opcache.so is already in the image, just add it to php.ini
 docker-php-ext-enable opcache
@@ -21,18 +21,23 @@ docker-php-ext-install -j$(nproc) mysqli
 docker-php-ext-install -j$(nproc) xsl
 docker-php-ext-install -j$(nproc) iconv
 docker-php-ext-install -j$(nproc) pdo_mysql
+docker-php-ext-configure imap --with-kerberos --with-imap-ssl
+docker-php-ext-install imap
+
+# Build Postgres extensions
+echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc |  apt-key add -
+apt-get update && apt-get install -y --no-install-recommends postgresql-server-dev-9.5 libpq5
 docker-php-ext-install -j$(nproc) pgsql
 docker-php-ext-install -j$(nproc) pdo_pgsql
 
-pecl install SPL_Types
-docker-php-ext-enable spl_types
 
 # Compile XDebug from github sources
 cd /usr/src/
-curl -fSL "https://xdebug.org/files/$XDEBUG_FILENAME" -o "$XDEBUG_FILENAME"
+wget https://xdebug.org/files/xdebug-2.4.0rc4.tgz
 mkdir /usr/src/xdebug
-tar -xf "$XDEBUG_FILENAME" --strip-components=1 -C /usr/src/xdebug
-rm "$XDEBUG_FILENAME"
+tar -xf xdebug-2.4.0rc4.tgz --strip-components=1 -C /usr/src/xdebug
+rm xdebug-2.4.0rc4.tgz
 cd xdebug
 phpize && ./configure && make -j"$(nproc)" && make install
 docker-php-ext-enable xdebug
@@ -46,7 +51,7 @@ phpize && ./configure --disable-memcached-sasl && make -j"$(nproc)" && make inst
 docker-php-ext-enable memcached
 
 
-# Generate a CLI version with SHM and PCNTL
+# Generate a CLI version with SHM, PCNTL and ZTS for pthreads
 cd $PHP_SOURCE_PATH
 ./configure \
     --with-config-file-path="$PHP_INI_DIR" \
@@ -54,15 +59,20 @@ cd $PHP_SOURCE_PATH
     --with-fpm-user=www-data --with-fpm-group=www-data \
     --disable-cgi --enable-ftp --with-zlib --enable-mysqlnd --with-readline --with-openssl --with-curl \
     --enable-sockets --enable-sysvsem --enable-sysvshm --enable-shmop --enable-posix --without-pear --enable-pcntl
-
 make -j"$(nproc)" && make install && make clean
+
+# Compile PECL extensions
+pear config-set preferred_state beta
+echo no| pecl install ev
+echo no| pecl install apcu
+
 
 #Clean up
 apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false -o APT::AutoRemove::SuggestsImportant=false \
     libbz2-dev libcurl4-openssl-dev libhashkit-dev libmemcached-dev libreadline6-dev libsasl2-dev libssl-dev \
     libtinfo-dev libxml2-dev libxslt1-dev zlib1g-dev \
-    libpq-dev init-system-helpers postgresql-client-common postgresql-common postgresql-server-dev-9.4 ssl-cert ucf \
+    libc-client2007e-dev libkrb5-dev \
+    libpq-dev init-system-helpers postgresql-client-common postgresql-common postgresql-server-dev-9.5 ssl-cert ucf \
     git
 
-rm -rf /var/lib/apt/lists/*
-rm -rf /usr/src/xdebug /usr/src/php-memcached
+rm -rf /var/lib/apt/lists/* /usr/src/xdebug /usr/src/php-memcached /tmp/*
